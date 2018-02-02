e83a58a565059b2f70eb6805c5fc914ead9ae15a
panchangzhi <panchangzhi@ziseyiliao.com>
2018-02-02 17:38:46 +0800

    pc 首页的swiper 切屏

--- src/views/pc.vue
+++ src/views/pc.vue
@@ -1,20 +1,55 @@
 <template>
-  <div class="pc-page">
-    <div class="barrage-wrapper">
+  <div class="pc-page" v-font>
+    <swiper :options="swiperOption">
+      <swiper-slide class="home slide">
+      </swiper-slide>
+      <swiper-slide class="lucky slide">
+      </swiper-slide>
+    </swiper>
+    <!-- <div class="barrage-wrapper">
       <barrage></barrage>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
 import barrage from '@/components/barrage-landscape.vue'
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 export default {
   components: {
-    barrage
+    barrage,
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        updateOnImagesReady: true,
+        height: window.innerHeight,
+        direction: 'vertical',
+        keyboard: true
+      }
+    }
   }
 }
 </script>
 <style lang="less" scoped>
+.pc-page{
+  height: 100%;
+  overflow: hidden;
+}
+.slide{
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100vw 100vh;
+  &.home{
+    background-image: url('../assets/images/bg-pc01.png')
+  }
+  &.lucky{
+    background-image: url('../assets/images/bg-pc02.png')
+  }
+}
 .barrage-wrapper{
   width: 300px;
   height: 400px;
